b4b8d6adf08f1f3e697322a7ef427dddfc5aec6c
Elliot Skogsberg Rydell <elliot.skogsberg@gmail.com>
2024-04-18 11:26:08 +0200

    remove rs cli

--- snake-rs-cli/target/debug/deps/snake_rs_cli-2a56af515c03257e.d
+++
@@ -1,5 +0,0 @@
-/home/atjowt/repo/snake/snake-rs-cli/target/debug/deps/libsnake_rs_cli-2a56af515c03257e.rmeta: src/main.rs
-
-/home/atjowt/repo/snake/snake-rs-cli/target/debug/deps/snake_rs_cli-2a56af515c03257e.d: src/main.rs
-
-src/main.rs:
--- snake-rs-cli/target/debug/snake-rs-cli.d
+++
@@ -1,1 +0,0 @@
-/home/atjowt/repo/snake/snake-rs-cli/target/debug/snake-rs-cli: /home/atjowt/repo/snake/snake-rs-cli/src/main.rs
